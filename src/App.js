5e04f497fb53aaa789954d8fc3695039ff018404
Guilherme C. Santos <guicarsiqsantos@gmail.com>
2022-04-03 21:52:20 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/static/data_shoes.json")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  if (!data || !data.length) return null;
+
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src="/static/images/caroussel.png" alt="Super Shoes Logo" />
+      </div>
+      <div className="carousel" ref={carousel}>
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image } = item;
+          return (
+            <div className="item" key={id}>
+              <div className="image">
+                <img src={image} alt={name} />
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+                <span className="oldPrice">R$ {oldPrice}</span>
+                <span className="price">R$ {price}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}>
+          <img src="/static/images/arrow-left.svg" alt="Scroll Left" />
+        </button>
+        <button onClick={handleRightClick}>
+          <img src="/static/images/arrow-left.svg" alt="Scroll Right" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
